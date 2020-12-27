c2ff9d6a43cd6ac5f5e436241394eda961d51481
NDR008 <nadir.syedsammut@gmail.com>
2020-12-27 16:23:34 +0100

    Debugger added too

--- get_started/main.c
+++ get_started/main.c
@@ -1,6 +1,8 @@
 /*
-
-Really basic hello world
+* Really basic hello world
+* Thanks to much work from the authors of pcsx-redux 
+* and the members of PSXDEV Netowork on discord
+*   Author of this setup: NDR008
 */
 
 #include <stdio.h>
